4cbcf4a62b2e0f8c27196ef6aef3ee45c40abfcf
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-04-27 17:41:47 +0600

    Declare function with params default value

--- function_with_params.php
+++ function_with_params.php
@@ -7,10 +7,13 @@ function sum($a, $b, $c){
     $num2 = $b;
     $num3= $c;
 
-    echo $num1+$num2 -$num3;
+    echo "($num1+$num2 -$num3) </br>";
 }
 
-sum(300,400,600);
+sum(300,400,600) ;
+sum(10,20,30);
+sum(1,2,3);
+
 
 
 ?>
---
+++ function_with_params_default_value.php
@@ -0,0 +1,14 @@
+<?php
+function sum($a, $b, $c=285){
+    $num1 = $a;
+    $num2 = $b;
+    $num3 = $c;
+
+    echo $num1+$num2-$num3;
+
+}
+
+sum(300,400);
+
+
+?>
