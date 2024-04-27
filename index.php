952bc2bbe123f3ef728abba5d0954caee7344e99
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-04-27 11:21:18 +0600

    Declare User/Coder Defined function

--- index.php
+++ index.php
@@ -12,7 +12,19 @@ echo $length;
 
 // String, Array, Date Time, CURL, SESSION, File,
 
-// 2. User Defined function
+// 2. User/Coder Defined function
+
+function sum(){
+    $num1=10;
+    $num2=20;
+    
+
+    echo $num1+$num2;
+}
+
+sum();
+
+
 
 
 ?>
